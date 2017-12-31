fe4f00d1bbb9f5b325cbdbde6359b100e99ca112
Andrew Sempere <a@feralresearch.org>
2017-12-31 23:22:25 +0100

    Fixes scope bug, adds color coding and verbose output for debugging

--- setPassword.js
+++ setPassword.js
@@ -4,6 +4,7 @@ const pg_password = process.env.SETPASSWORD_PG_PASSWORD;
 const pg_database = process.env.SETPASSWORD_PG_DATABASE;
 
 const pg_url = "postgresql://"+pg_host+"/"+pg_database+"?user="+pg_user+"&password="+pg_password+"&ssl=false";
+console.log("\x1b[1m\nDatabase: \x1b[41m%s\x1b[0m",pg_url);
 const argv = require('minimist')(process.argv.slice(2));
 const bcrypt = require('bcrypt');
 const pg = require('pg');
@@ -24,8 +25,9 @@ if(( ( argv.u || argv.e || argv.i) && argv.p)){
 				console.log("--u option requires that you specify a username");
 				process.exit()
 			}
-			var sql = "UPDATE users SET encrypted_password = '"+hash+"' WHERE username = '" +this.username+ "'";
-			console.log("Changing password for '"+username+"' to '"+newPassword+"'");
+			var sql = "UPDATE users SET encrypted_password = '"+hash+"' WHERE username = '" +username+ "'";
+			msg = "Changing password for '"+username+"' to '"+newPassword+"'";
+
 
 		}else if (argv.e){
 			var email = argv.e.toString().trim();
@@ -33,8 +35,8 @@ if(( ( argv.u || argv.e || argv.i) && argv.p)){
 				console.log("--e option requires that you specify an email");
 				process.exit()
 			}
-			var sql = "UPDATE users SET encrypted_password = '"+hash+"' WHERE email = '" +this.email+ "'";
-			console.log("Changing password for '"+email+"' to '"+newPassword+"'");
+			var sql = "UPDATE users SET encrypted_password = '"+hash+"' WHERE email = '" +email+ "'";
+			msg = "Changing password for '"+email+"' to '"+newPassword+"'";
 
 		}else if (argv.i){
 			var id = argv.i.toString().trim();
@@ -42,20 +44,22 @@ if(( ( argv.u || argv.e || argv.i) && argv.p)){
 				console.log("--i option requires that you specify a user id");
 				process.exit()
 			}
-			var sql = "UPDATE users SET encrypted_password = '"+hash+"' WHERE id = '" +this.id+ "'";
-			console.log("Changing password for user ID '"+id+"' to '"+newPassword+"'");
+			var sql = "UPDATE users SET encrypted_password = '"+hash+"' WHERE id = '" +id+ "'";
+			msg = "Changing password for user ID '"+id+"' to '"+newPassword+"'";
+
 		}else{
 			process.exit();
 		}
-		executeSQL(sql);
+		executeSQL(sql, msg, hash);
 
-	}.bind( {
-			} ));
+
+	}.bind( {} ));
+
 }else{
-	console.log('USAGE: setPassword [--u USERNAME | --e EMAIL | --i ID] --p NEWPASSWORD');
+	console.log('\x1b[36m%s\x1b[0m','USAGE: setPassword [--u USERNAME | --e EMAIL | --i ID] --p NEWPASSWORD');
 }
 
-var executeSQL = function(sql) {
+var executeSQL = function(sql, msg, hash) {
 	var client = new pg.Client(pg_url);
 	client.connect(function(err) {
 	  if(err) {
@@ -65,7 +69,13 @@ var executeSQL = function(sql) {
 	    if(err) {
 	      return console.error('ERROR: query failed!', err);
 	    }
-	    //console.log(result);
+		console.log("\x1b[2m\x1b[37m---------------------------------------------------------------------");
+		console.log(sql);
+		console.log("---------------------------------------------------------------------");
+		console.log(result);
+		console.log("---------------------------------------------------------------------");
+		console.log('\x1b[2m\x1b[37m%s\x1b[0m','New hash: '+hash);
+		console.log('\n\x1b[0m\x1b[37m%s\x1b[0m',msg);
 	    client.end();
 	  });
 	});
